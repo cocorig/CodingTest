3f09f0f09269d71cb5bb572fcca3727a0befa19d
cocorig <95855640+cocorig@users.noreply.github.com>
2023-05-10 23:58:17 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,11 @@
+function solution(num1, num2) {
+
+   if (num1 === num2) { 
+       return 1;
+   }else {
+       return -1;
+   }
+    return answer; //실행 될 가능성? 없다
+}
+//1. 두 수가 같은지 비교
+//2. 비교 결과에 따라 -1, 1
