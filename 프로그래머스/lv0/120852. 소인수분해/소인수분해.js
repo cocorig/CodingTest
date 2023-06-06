ca12c0cc68dedadaaacd3d6735ec7e1e1824d9a8
Hayeon LEE <95855640+cocorig@users.noreply.github.com>
2023-06-06 17:09:48 +0900

    [level 0] Title: 소인수분해, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,18 +1,19 @@
 function solution(n) {
-   let prime = [];
-    
-    for(let i=2; i<= Math.sqrt(n); i++){
-        while(n % i === 0){
-            prime.push(i);
-            
-            n /= i;
-           
-        }
-    }
-    if(n >= 2){
-        prime.push(n)
-    }
-    let ans = Array.from(new Set(prime));
-    
-    return ans.sort((a,b) => a -b);
+   
+//1.n을 가장작은 소수인 2부터 나눈다.
+//2. 나머지가 0이면 그 소수는 정수의 소인수이다.
+//3. 나눈 결과를 다시 2로나누고 정수가 소인수로 나눌 수 없을때까지 계속 나누고 , 정수가 1이 되면 소인수 완료.
+    let arr = [];
+    let i =  2; // i를 초기화하고 반복해야함!!!!
+    while(n > 1){//n이 12부터시작해 1보다 크거나 같을때 까지 반복
+        if(n % i === 0){
+          arr.push(i)
+          n = n / i ;
+      }else{
+          i++;
+      }
+       
+   }
+   let ans =  Array.from(new Set(arr));
+   return ans.sort((a,b)=> a - b)
 }
