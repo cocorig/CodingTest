cb938073cba8bfdc7585963f0d3c3381639ec55c
Hayeon LEE <95855640+cocorig@users.noreply.github.com>
2023-06-11 16:50:31 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,21 @@
+function solution(n) {
+    
+    
+   let ans = 0;
+    
+    for(let i = 1; i <= n; i++){
+        ans += 1;
+        
+        while(true){
+            if(ans % 3 === 0 || String(ans).includes('3')){
+                ans += 1;
+                
+                continue;
+            }
+            
+            break;
+        }
+    }
+    
+  return(ans)
+}
