56bead5d2f20945178b7ec1b03db0cdddb94e209
Hayeon LEE <95855640+cocorig@users.noreply.github.com>
2023-06-11 20:01:05 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -2,9 +2,14 @@ function solution(numlist, n) {
 
 
     
-let num =  numlist.sort((a,b)=>{ 
-      return  Math.abs(a - n) - Math.abs(b - n)  || b - a   
-    })
+return  numlist.sort((a,b)=>
+     // (a - b)이기때문에 오름차순으로 정렬->차가 작을수록 가까운수
+    Math.abs(a - n) - Math.abs(b - n) || 
+        // ||을 썻기때문에
+        //차가 같다면(앞의 결과가 0이면) 더 큰게 앞으로 -> 내림차순해라
+        
+        b - a   
+    );
 
-return (num)
+
 }
