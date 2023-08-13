2aa3d0d90038d7358b48910579d627a4c53578cd
Hayeon LEE <fhdodl@naver.com>
2023-08-13 21:33:43 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,9 +1,9 @@
 function solution(numbers) {
-    
-    let sum =0;
-    for(let i =0; i <numbers.length;i++){
-    sum =  sum + numbers[i]
-        
-    }
-    return sum / numbers.length;
+  let sum = 0;
+  
+  numbers.forEach(item => {
+    sum += item;
+  });
+  
+  return sum / numbers.length;
 }
