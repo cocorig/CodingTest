d6f278943732683c0abc2c29ca9fa13e7a1af8d9
Hayeon LEE <fhdodl@naver.com>
2023-08-13 23:11:52 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,11 +1,7 @@
-//1.numbers 배열의 원소를 꺼냄
-//2.원소를 꺼내 2배를 한 후 새 배열에 넣어줌-> answer.push()->push눈 몇번?? 배열의 길이 만큼
-function solution(numbers) {
-    let answer = [];
-    for(let i=0; i < numbers.length; i++ ){
-        answer.push(numbers[i] * 2);
-        console.log(i)
-    }
 
-    return answer;
+function solution(numbers) {
+   const ans =  numbers.map(item =>{
+  return item * 2
+})
+   return ans;
 }
