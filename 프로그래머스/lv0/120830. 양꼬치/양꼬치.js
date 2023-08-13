3741db57f171c66082bfa5ce404e7eaa6c38241c
Hayeon LEE <fhdodl@naver.com>
2023-08-13 20:57:23 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,9 +1,6 @@
-function solution(n, k) {
-    let num = 0;
-    let result=0;
-    if(n >= 10){
-        num = Math.floor(n / 10);
-    }
-    result = (n * 12000) + (k * 2000) - (num * 2000);
-    return result;
+function solution(n , k){
+  const serviceDrink = Math.floor(n / 10);
+  let result = 
+  (n * 12000) + (k * 2000) - (serviceDrink * 2000)
+  return result;
 }
