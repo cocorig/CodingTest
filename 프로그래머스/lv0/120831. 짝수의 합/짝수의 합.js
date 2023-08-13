fed0505d1760d09095fa58225cf9db3baa69227e
Hayeon LEE <fhdodl@naver.com>
2023-08-13 21:15:34 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,11 +1,10 @@
-function solution(n) {
-    //먼저 짝수를 구하고 더해준다 누적시킴
-    let sum = 0;
-    for(let i=1; i <=n; i++){
-       if(i % 2 ===0){
-           sum += i;
-       }
-       
+
+function solution(n){
+  let result = 0;
+  for(let i=1; i<=n; i++){ 
+    if(i % 2 === 0){
+      result += i;
     }
-    return sum;
+}
+    return result;
 }
