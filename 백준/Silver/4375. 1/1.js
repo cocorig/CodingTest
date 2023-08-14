1bb1f2fc2b1459a65793310423574d8e24495137
Hayeon LEE <fhdodl@naver.com>
2023-08-14 15:03:54 +0900

    [Silver III] Title: 1, Time: 180 ms, Memory: 11176 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.js
+++ 백준/Silver/4375. 1/1.js
@@ -1,21 +1,35 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-function findMinimumOnesMultiple(n) {
-    let num = '1';
-    let remainder = 1 % n;
 
-    while (remainder !== 0) {
-        num += '1';
-        remainder = (remainder * 10 + 1) % n;
-    }
+// 1.  각 자릿수가 모두 1로만 이루어진 n의 배수를 찾는다.
+// 2.  배수의 자리수를 출력한다.
 
-    return num.length;
+function solution(n){
+
+  let remainder = 1 % n;
+  let num = '1';
+  
+  while(remainder !== 0){ 
+    num += '1';
+    remainder =  (remainder * 10 + 1) % n;
+  
+  }
+  return num.length;
 }
 
-for (let i = 0; i < input.length; i++) {
-    const n = parseInt(input[i]);
-    const result = findMinimumOnesMultiple(n);
-    console.log(result);
+
+for(let i = 0; i < input.length; i++){
+
+const ans =   solution(parseInt(input[i]));  //3
+console.log(ans);
 }
+
+
+// 1 % n 
+// 11 % n 
+// 111 % n 
+// 1111 % n 
+// 1111 % n 
+// ---- 0이 될때 n의 배수가 됨
