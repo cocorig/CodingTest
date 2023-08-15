45d3792499df3a087c94632050dacc935e7484aa
Hayeon LEE <fhdodl@naver.com>
2023-08-15 17:10:15 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,8 +1,7 @@
-function solution(my_string) {
-    let str = '';
-    for(let i = my_string.length-1; i >=0; i--){
-        str += my_string.charAt(i)
-        
-    }
-   return str;
+
+function solution(my_string){
+  return  my_string.split('').reverse().join('');
+
 }
+
+
