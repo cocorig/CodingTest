5bc578cd358a0a7b235b6cf546a4c427cb5ebec0
Hayeon LEE <fhdodl@naver.com>
2023-08-15 18:33:30 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,17 +1,14 @@
 function solution(rsp) {
-    const str = [];
-    
-    for(let i = 0; i < rsp.length; i++){
-       
-        if(rsp[i] == 2){
-            str.push(0);
-        }else if(rsp[i] == 0){
-            str.push(5);
-        }else if(rsp[i] == 5){
-            str.push(2)
-        }
-       
+  let ans = '';
+  for(let i=0; i < rsp.length; i++) {
+  
+    if(rsp[i] === '2') {
+      ans += '0';
+    }else if( rsp[i] === '0'){
+      ans += '5';
+    }else if(rsp[i] === '5'){
+      ans +=  '2';
     }
-    
-    return str.join('');
+  }
+ return (ans);
 }
