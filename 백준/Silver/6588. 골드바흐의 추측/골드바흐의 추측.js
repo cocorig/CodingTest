80367dac08e70e0418fa794fe5ca7a4a997c5a95
Hayeon LEE <fhdodl@naver.com>
2023-08-19 17:09:14 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 444 ms, Memory: 62996 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.js
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.js
@@ -2,31 +2,34 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n').map(Number);
 
-// console.log(input);
-
+let result = '';
 // 주어진 수보다 작은 소수 중에 가장 큰 수 찾기
 const max = Math.max(...input);
 
-// 4보다 큰 모든 짝수는 두 홀수 소수의 합으로 나타낼 수 있다.
+let isPrime = new Array(max + 1).fill(true);
 
-// 주어진 수보다 작은 소수 중에 가장 큰 수 찾기
+isPrime[0] = false;
+isPrime[1] = false;
 
-const largestNum = Math.max(...input);
-const check = new Array(largestNum + 1).fill(false);
-for (let i = 2; i <= largestNum; i++) {
-  if (!check[i]) {
-    for (let j = i * i; j <= largestNum; j += i) {
-      check[j] = true;
+for (let i = 2; i * i <= max; i++) {
+  if (isPrime[i]) {
+    let multiple = 2;
+    while (i * multiple <= max) {
+      isPrime[i * multiple] = false;
+      multiple++;
     }
   }
 }
-let result = "";
-input.map((x) => {
-  for (let i = 3; i < x; i += 2) {
-    if (!check[i] && !check[x - i]) {
-      result += `${x} = ${i} + ${x - i}\n`;
-      break;
+
+input.forEach((item) => {
+  if (item >= 4 && item % 2 === 0) {
+    for (let i = 2; i <= item / 2; i++) {
+      if (isPrime[i] && isPrime[item - i]) {
+        result += `${item} = ${i} + ${item - i}\n`;
+        break;
+      }
     }
   }
 });
+
 console.log(result);
