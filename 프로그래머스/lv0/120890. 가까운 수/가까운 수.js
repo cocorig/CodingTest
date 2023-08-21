6bd39fa6d0839b981bb82f416c8b517e33ad0f75
Hayeon LEE <fhdodl@naver.com>
2023-08-21 16:25:30 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,14 +1,12 @@
 function solution(array, n) {
-    array.sort((a,b)=> a-b);
-    //console.log(array)
-    let arr =[]
-    let a = 0;
-    let b = 0;
-    for(let i = 0; i< array.length; i++){
-       arr.push(Math.abs(n - array[i]))
-       a =  Math.min(...arr) //가장 작은값
-       b = arr.indexOf(a) //최소값이 담긴 index를 찾아라 
-       
-    }
-    return (array[b])
+  let arr = []
+  array.sort((a,b)=> a - b);
+  for(let i=0; i< array.length; i++){
+  arr.push(Math.abs(n - array[i])) ; 
+} //arr = [ 17, 10, 8 ] -> n에 각 원소를 뺀 값이 들어있는 배열
+// 제일 작은 수를 min변수에 담음
+let min  = Math.min(...arr);  // 8
+// min값의 인덱스가 기본배열의 가까운 수가 들어있는 원소자리이기 때문에 인덱스를 찾음
+let indx =  arr.indexOf(min)
+return array[indx];
 }     
