7c6380f2fa8ef5c1fc627153e73790580a2b2444
Hayeon LEE <fhdodl@naver.com>
2023-08-21 23:39:43 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,15 +1,11 @@
-function solution(emergency) {
-    //내림차순으로 정렬
-    const emr =  [...emergency].sort((a,b)=>b-a);
-    //진료순위를 기록할 배열
-    let rankArr =  new Array(emergency.length).fill(0);
-    let cnt = 1;//진료순위
-    for(let i=0; i< emergency.length; i++){
-        //원래배열의 index를 구한다.
-        let index = emergency.indexOf(emr[i]);//1
-        rankArr.splice(index,1, cnt);
-        
-        cnt++;
-    }
-    return rankArr;
+function solution(emergency){             
+
+  let sortEmergency = [...emergency].sort((a,b)  =>  b - a);
+
+  let ans = [];
+  for(let i = 0; i < emergency.length; i++){
+    let idx = sortEmergency.indexOf(emergency[i]);
+    ans.push(idx + 1);
+  }
+  return ans;
 }
