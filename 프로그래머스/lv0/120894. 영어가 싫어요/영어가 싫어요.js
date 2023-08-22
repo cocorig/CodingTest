42312bf8d43270d571116f4997946001c5405463
Hayeon LEE <fhdodl@naver.com>
2023-08-23 01:52:34 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,9 +1,11 @@
-function solution(numbers) {
-    const numArr = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-    let ans;
-    numArr.forEach((item , index)=>{
-       numbers =  numbers.split(item).join(String(index)); // itme기준으로 나눠짐
-        //console.log(ans)
-    }) //item기준으로 나눈 그 자리에 join으로 index넣어줌
-    return Number(numbers)
+
+
+function solution(numbers){
+  const number = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+  number.forEach((v , i)=>{
+    numbers =  (numbers.split(v).join(i)); // number원소로 구분하면 그 자리는 빈 공간이 생김 ,빈 공간을 같은 문자가 있는 number의 인덱스로 채움 -> 
+    
+  })
+  return Number(numbers);
 }
