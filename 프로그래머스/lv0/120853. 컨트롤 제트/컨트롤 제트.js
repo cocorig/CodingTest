680d37326a31af8eb31549ca76a0d2affa742a1f
Hayeon LEE <fhdodl@naver.com>
2023-08-25 01:06:29 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,23 +1,14 @@
-function solution(s) {
-    
-  
-    const input = s.split(' ');
-    let stack = [];
-    let sum = 0;
-    for(let i = 0; i <  input.length; i++){
-        if(input[i] === 'Z'){
-            stack.pop();
-            
-            continue;//Z일때 z는 배열에 넣지 않기위해 다시 반복문으로 돌아간다.
-        }
-        stack.push(input[i]);//z가 아닐때
+
+function solution(s){
+  const arr = s.split(" ")
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i]  === 'Z'){
+      sum -= parseInt(arr[i-1]);
+    }else{
+      sum += parseInt(arr[i]);
     }
-     console.log(stack)
     
-    stack.map((n)=>{
-       let num =  Number(n); //1,3
-        sum += num; //1 + 3 =
-        
-    })
-    return sum;
+  }
+  return sum;
 }
