a98bd4fe230d6916eb5497e9ff3fb1c34a2e1e00
Hayeon LEE <fhdodl@naver.com>
2023-09-23 05:56:25 +0900

    [level 2] Title: 올바른 괄호, Time: 8.29 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,23 +1,17 @@
-function solution(s){
-    if(s[0] === ')'){
-        return false;
-    }
-    
-    if(s[s.length - 1] === '('){
-        return false;
-    }
-    
-    let stack = [];
-    
-    for(let i = 0; i < s.length; i++){
-        stack.push(s[i]);
-        
-        if(stack[stack.length - 2] === '(' && stack[stack.length - 1] === ')'){
-            stack.pop();
-            stack.pop();
-        }
-    }
-    
-      return stack.length > 0  ?  false : true;
-}
+function solution(s) {
+  s = s.split("");
 
+  let stack = [];
+  for (let i of s) {
+    if (i === "(") {
+      stack.push(i);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+}
