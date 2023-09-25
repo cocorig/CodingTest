a99b3faa0a81acedd49676266ab43be4da897fea
Hayeon LEE <fhdodl@naver.com>
2023-09-25 17:36:46 +0900

    [level 2] Title: 올바른 괄호, Time: 5.72 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,5 +1,7 @@
-function solution(s){
-   if(s[0] === ')'){
+function solution(s) {
+
+  // 맨 앞의 문자가 닫기괄호일 경우 바로 false반환
+  if(s[0] === ')'){
     return false;
   }
   let steck = [];
@@ -7,12 +9,12 @@ function solution(s){
     if(s[i] === '('){
       steck.push(s[i]);
     }else{
-    
+      //')' 닫기괄호인데  steck안의 원소가 없을 경우는 올바르지 않은 괄호이기 때문에 바로 false 반환
       if(steck.length === 0){
         return false;
       }
       steck.pop();
     }
   }
-  return steck.length === 0; //
+  return steck.length === 0; // 0이면 true , 아니면 false를 바로 반환하게
 }
