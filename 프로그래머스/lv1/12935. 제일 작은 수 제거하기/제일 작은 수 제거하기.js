254fa586e80890955ba207af6319ffa135d6f9e7
Hayeon LEE <fhdodl@naver.com>
2023-09-28 18:53:18 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  if(arr.length <= 1) {
+    return [-1];
+  }
+  let min = Math.max(...arr);
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  const ans =  arr.filter((item ,  index)=> item !== min)
+  return ans;
+}
