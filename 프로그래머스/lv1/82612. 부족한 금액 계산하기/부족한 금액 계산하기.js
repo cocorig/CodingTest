c56ef19357a8590eb5adc370305a52868c168c09
Hayeon LEE <fhdodl@naver.com>
2023-10-03 08:53:24 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+      let moneyRequired = 0;
+  for(let i = 1; i <= count; i++){
+    moneyRequired += price * i;
+  }
+ return  (money - moneyRequired) < 0 ?  Math.abs(money - moneyRequired) : 0
+ 
+}
