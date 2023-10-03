884336b8c587ebddae244e7789d264cd03c23e80
Hayeon LEE <fhdodl@naver.com>
2023-10-03 09:32:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,13 @@
+function solution(n, m) {
+   
+   let lcm  = n * m;
+  let a = Math.max(n, m);
+  let b = Math.min(n, m);
+
+  while(b !== 0){
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return [a , lcm / a]
+}
