359e40f88f39ca9c1d64d5d64ffa08c7a0701f22
Hayeon LEE <fhdodl@naver.com>
2023-11-15 00:03:53 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.61 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,54 +1,46 @@
-// 개인정보의 만료일을 계산하는 함수 , 복잡한 로직을 따로 함수로 빼서 씀
-function getExpiredDate(dataString, month) {
-    // 주어진 날짜 문자열을 숫자로 변환하여 분리
-    let [year, mon, day] = dataString.split('.').map(Number);
-    
-    // 주어진 개월 수를 현재 월에 더함
-    mon += month;
-
-    // 월이 12를 넘어갈 경우 연도를 조정
-    while (mon > 12) {
-        mon -= 12;
-        year += 1;
-    }
-
-    // 해당 월의 마지막 날을 계산
-    const lastDayOfMonth = new Date(year, mon, 0).getDate();
-    
-    // 만료일(day)를 해당 월의 마지막 날 또는 주어진 일(day) 중 작은 값으로 조정
-    day = Math.min(day, lastDayOfMonth);
-
-    // 조정된 연도, 월, 일을 배열로 반환
-    return [year, mon, day];
-}
-
-// 주어진 약관과 개인정보를 통해 만료된 개인정보를 찾는 함수
 function solution(today, terms, privacies) {
+    // 입력된 오늘 날짜를 숫자로 변환 (예: "2022.05.19" -> 20220519)
+    const todayDate = Number(today.split('.').join(''));
+
+    // 약관 정보를 담을 객체 생성
+    let termsObj = {};
+
+    // 약관 정보를 객체에 저장 (예: ["A 6", "B 12", "C 3"] -> { A: 6, B: 12, C: 3 })
+    terms.forEach((term) => {
+        const [contract, month] = term.split(' ');
+        termsObj[contract] = Number(month);
+    });
+
     // 결과를 저장할 배열
-    const answer = [];
+    let result = [];
 
-    // 개인정보 배열을 순회하면서 처리
+    // 개인정보를 처리하면서 만료일을 계산
     privacies.forEach((privacy, index) => {
-        // 개인정보 문자열을 날짜와 약관으로 분리
-        const [data, term] = privacy.split(" ");
-        
-        // 약관에서 유효기간을 찾아 숫자로 변환
-        const termPeriod = Number(terms.find(innerTerm => innerTerm.startsWith(term)).split(' ')[1]);
+        let [date, contract] = privacy.split(' ');
 
-        // 만료일을 계산
-        const [eYear, eMon, eDay] = getExpiredDate(data, termPeriod);
-        // 현재 날짜를 계산
-        const [year, mon, day] = today.split('.').map(Number);
+        // 날짜를 연, 월, 일로 분리하고 월에 약관에서 가져온 개월 수를 더함
+        let [year, month, day] = date.split('.').map(Number);
+        month += termsObj[contract];
 
-        // 만료일과 현재 날짜를 비교하여 만료된 경우 배열에 추가
-        const expirationDate = new Date(eYear, eMon - 1, eDay);
-        const currentDate = new Date(year, mon - 1, day);
+        // 월이 12를 넘어갈 경우 연도를 조정
+        while (month > 12) {
+            year += 1;
+            month -= 12;
+        }
 
-        if (currentDate > expirationDate || currentDate.toDateString() === expirationDate.toDateString()) {
-            answer.push(index + 1);
+        // 연, 월, 일을 문자열로 변환하고 자릿수를 맞춤
+        year = String(year);
+        month = String(month).padStart(2, '0');
+        day = String(day).padStart(2, '0');
+
+        // 계산된 날짜를 숫자로 변환 (예: "20220519" -> 20220519)
+        const calculatedDate = Number(year + month + day);
+
+        // 만료일이 현재 날짜 이전이면 결과 배열에 추가
+        if (calculatedDate <= todayDate) {
+            result.push(index + 1);
         }
     });
 
-    // 만료된 개인정보의 번호를 반환
-    return answer;
+    return result;
 }
