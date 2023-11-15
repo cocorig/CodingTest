fc7a54b7bf90f64eed2a4491116ba152acaa4fe2
Hayeon LEE <fhdodl@naver.com>
2023-11-15 23:40:43 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+     
+    const reversedTernary = n.toString(3).split('').reverse().join('');
+    
+  
+    const answer = parseInt(reversedTernary, 3);
+
+    return answer;
+}
