91e658849241a0ee7b684d088e2c899d14c06871
Hayeon LEE <fhdodl@naver.com>
2023-11-21 00:15:49 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,10 +1,16 @@
 function solution(numbers) {
-    const sumSet = new Set(); // 중복제거를 위해 사용함
-    numbers.forEach((number , index)=>{
-    console.log(number , index)
-      numbers.slice(index + 1).forEach(nextNum =>{
-          sumSet.add(number + nextNum);
-      })
-})
-return [...sumSet].sort((a , b)=> a - b)
+    const result = [];
+
+    numbers.sort((a, b) => a - b);
+
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            const sum = numbers[i] + numbers[j];
+            if (!result.includes(sum)) {
+                result.push(sum);
+            }
+        }
+    }
+
+    return result.sort((a, b) => a - b);
 }
