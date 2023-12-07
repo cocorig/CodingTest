8a4b7d1ed33817c3af1bb1522c72c6d5f33f68b1
Hayeon LEE <fhdodl@naver.com>
2023-12-07 12:04:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,20 +1,15 @@
 function solution(number, k) {
-    const stack = [];
+  const stack = [];
 
-    for (let i = 0; i < number.length; i++) {
-        const current = number[i];
-
-     
-        while (stack.length > 0 && stack[stack.length - 1] < current && k > 0) {
-            stack.pop();
-            k--;
-        }
-
-        stack.push(current);
+  for (const num of number) {
+    while (k > 0 && stack[stack.length - 1] < num) {
+      stack.pop();
+      k--;
     }
+    stack.push(num);
+  }
 
+  stack.splice(stack.length - k, k);
 
-    stack.splice(stack.length - k, k);
-
-    return stack.join('');
+  return stack.join("");
 }
