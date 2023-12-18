f418e31d4cf29611ac1d1ec5369a19f5b3846561
Hayeon LEE <fhdodl@naver.com>
2023-12-18 13:29:33 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,21 @@
+function solution(new_id) {
+  new_id = new_id.toLowerCase();
+  new_id = new_id.replace(/[^a-z0-9\-_.]/g, "");
+  new_id = new_id.replace(/\.+/g, ".");
+  new_id = new_id.replace(/^\.|\.$/g, "");
+  if (new_id === "") {
+    new_id = "a";
+  }
+  if (new_id.length >= 16) {
+    new_id = new_id.slice(0, 15);
+       new_id = new_id.replace(/^\.|\.$/g, "");
+  }
+
+  if (new_id.length <= 2) {
+    const c = new_id.slice(-1);
+    while (new_id.length < 3) {
+      new_id += c;
+    }
+  }
+  return new_id;
+}
