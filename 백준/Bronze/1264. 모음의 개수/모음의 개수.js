b998eeca6c0d175fd58fbb3d5b092e7e73588800
Hayeon LEE <fhdodl@naver.com>
2023-12-19 00:05:05 +0900

    [Bronze IV] Title: 모음의 개수, Time: 128 ms, Memory: 9424 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.js
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.js
@@ -3,7 +3,7 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
 for (let i = 0; i < input.length; i++) {
-  if (input[i].trim() === "#") {
+  if (input[i] === "#") {
     break;
   } else {
     const vowels = input[i].match(/[aeiou]/gi);
