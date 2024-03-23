f5d664daa38373316e763fdd5bb4ef128e03f0de
Hayeon LEE <fhdodl@naver.com>
2024-03-23 20:47:34 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,12 +1,8 @@
 function solution(citations) {
   citations.sort((a, b) => b - a);
-  let hIndex = 0;
-  for (let i = 0; i < citations.length; i++) {
-    if (citations[i] >= i + 1) {
-      hIndex = i + 1;
-    } else {
-      break;
-    }
+  let h = 0;
+  while (citations[h] >= h + 1) {
+    h++;
   }
-  return hIndex;
+  return h;
 }
