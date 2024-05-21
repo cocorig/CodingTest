bb5c01e3fd4aadfaeffba8e127eaee1178e547f4
Hayeon LEE <fhdodl@naver.com>
2024-05-22 00:33:02 +0900

    [Bronze III] Title: ACM 호텔, Time: 144 ms, Memory: 9860 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
@@ -9,7 +9,13 @@ for (let i = 1; i <= testData; i++) {
   const H = row[0];
   const W = row[1];
   const N = row[2];
-  const line = Math.ceil(N / H);
-  const floor = N - H * (line - 1);
-  console.log(floor * 100 + line);
+
+  let floor = N % H;
+  let roomNumber = Math.ceil(N / H);
+
+  if (floor === 0) {
+    floor = H;
+  }
+
+  console.log(floor * 100 + roomNumber);
 }
