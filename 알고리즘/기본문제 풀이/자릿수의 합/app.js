b2ca9086ff1a44d6e4b4f10130af029c32211238
Hayeon LEE <fhdodl@naver.com>
2024-05-23 23:54:49 +0900

    feat: 완전탐색_자릿수의 합

---
+++ 알고리즘/기본문제 풀이/자릿수의 합/app.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let sum = x
+      .toString()
+      .split("")
+      .reduce((a, b) => a + Number(b), 0);
+
+    if (max < sum) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(arr));
