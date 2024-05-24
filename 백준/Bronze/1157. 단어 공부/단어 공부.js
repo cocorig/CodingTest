610468565aff2add1e171d037647092e03d58e38
Hayeon LEE <fhdodl@naver.com>
2024-05-24 18:49:51 +0900

    [Bronze I] Title: 단어 공부, Time: 324 ms, Memory: 15620 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -1,40 +1,29 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
 
+let obj = {};
+for (let i = 0; i < input.length; i++) {
+  let char = input[i].toUpperCase();
 
-
-solution(input[0]);
-
-function solution(str) {
-    let obj ={};//각각 문자가 몇번씩 등장했는지 저장해주는 객체
-    for(let i=0; i<str.length; i++){
-
-        let nowChar = str[i].toUpperCase(); //M
-        if(obj[nowChar]){
-            obj[nowChar]++; //만약 객체에 N의 값이 이미 등장했었다면 +1증가
-        }else{
-            obj[nowChar]=1;
-        }
-    }
-    let max = -1; //최대값 저장
-    let maxChar ='';
-    let isDuplicated = false; //onj[el]
-    for( const el in obj){   //obj의 키값,onj[el]= 1,4,4,1
-        if (obj[el]> max){  //{ M: 1, I: 4, S: 4, P: 1 }
-            max = obj[el]; // el은 문자
-            maxChar =el;
-            isDuplicated = false; //중복아님
-        }else if( obj[el]===max){//현재 검사하는 키값이 최대값의 키값과 동일하면 중복이 된거때문에 
-            isDuplicated =true; //중복임
-        }
-    }
-    if(isDuplicated){   //만약 중복이면 ?출력
-        console.log('?');
-    }else{    //아니면 키값의 문자 출력
-        console.log(maxChar);
-    }
-
-
-
+  obj[char] ? obj[char]++ : (obj[char] = 1);
+  // 처음은 1로 설정,0으로 설정하면 없으면 0이되기 때문에  있는 상태가 되지않는다. 다음에 또 나와도 0으로 설정되있으면 없는값으로 간주하기 때문이다.
+}
+// 뒤에 더 큰 값이 있을 수 있으니까 바로 ?를 리턴하는건 예외상황에 맞지않음
+let max = Number.MIN_SAFE_INTEGER;
+let maxChar = "";
+let isDuplicated = false;
+for (char in obj) {
+  if (max < obj[char]) {
+    max = obj[char];
+    maxChar = char;
+    isDuplicated = false;
+  } else if (max === obj[char]) {
+    isDuplicated = true;
+  }
+}
+if (isDuplicated) {
+  console.log("?");
+} else {
+  console.log(maxChar);
 }
