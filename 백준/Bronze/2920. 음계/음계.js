25a62dd4f8bf769074baedec7c1db1ec8a75c2ed
Hayeon LEE <fhdodl@naver.com>
2024-05-26 23:11:17 +0900

    [Bronze II] Title: 음계, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+let compareArr = input.slice();
+
+if (compareArr.sort((a, b) => a - b).join(" ") === input.join(" ")) {
+  console.log("ascending");
+} else if (compareArr.sort((a, b) => b - a).join(" ") === input.join(" ")) {
+  console.log("descending");
+} else {
+  console.log("mixed");
+}
