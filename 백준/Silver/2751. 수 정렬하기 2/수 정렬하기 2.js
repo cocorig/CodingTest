4b478caad9d6822acbef260c01ae520f1a505c6d
Hayeon LEE <fhdodl@naver.com>
2024-05-29 23:40:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1304 ms, Memory: 218004 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const arr = input.slice(1);
+
+console.log(arr.sort((a, b) => a - b).join("\n"));
